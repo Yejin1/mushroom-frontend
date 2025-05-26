b503ee14ad63b76e46fa136f78abb99d07dded3e
Yejin1 <dpwls273273@gmail.com>
2025-05-26 23:14:25 +0900

    회원가입 파일생성

---
+++ src/routes/Join.jsx
@@ -0,0 +1,22 @@
+//  Join.jsx
+/**
+ * - 회원가입
+ */
+
+import React, {useEffect, useState} from "react"
+import axios from 'axios'
+
+function Join() {
+
+
+
+  return (
+    <div className="login-page">
+      <form>
+
+      </form>
+    </div>
+  )
+}
+
+export default Join
